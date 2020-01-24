2ab469d2257590b5bef9847ee96553537f2cadeb
Mark Cornelio <markcornelio28@gmail.com>
2020-01-24 14:15:38 +0800

    added new fix

---
+++ build/precache-manifest.f47d431efd48938199879548e873060f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e28d1545885f6507e8bbdedea21ee1",
+    "url": "/dragonslayer-react/index.html"
+  },
+  {
+    "revision": "2a6c0ec55475fc1d786b",
+    "url": "/dragonslayer-react/static/css/main.9ca5cffe.chunk.css"
+  },
+  {
+    "revision": "3cc437c8f93c4e691209",
+    "url": "/dragonslayer-react/static/js/2.efddd928.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/dragonslayer-react/static/js/2.efddd928.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a6c0ec55475fc1d786b",
+    "url": "/dragonslayer-react/static/js/main.ce14bed7.chunk.js"
+  },
+  {
+    "revision": "5451e8edcfdced50caed",
+    "url": "/dragonslayer-react/static/js/runtime-main.60ea269e.js"
+  },
+  {
+    "revision": "33221ae962881a689d95a6f35a415f09",
+    "url": "/dragonslayer-react/static/media/dragon.33221ae9.png"
+  }
+]);
